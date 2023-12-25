295e67ded3f5082b422cdadc613e138c2ecacb67
MarcDagher <marcdagher47@gmail.com>
2023-12-25 16:56:18 +0200

    adjust conditions for send_request

--- laravel_bh/app/Http/Controllers/ConnectionsController.php
+++ laravel_bh/app/Http/Controllers/ConnectionsController.php
@@ -9,11 +9,12 @@ use Illuminate\Http\Request;
 
 class ConnectionsController extends Controller
 {
-    // query the records with accepted or disconnected related to the user
     public function display_history (){
+        // query the records with accepted or disconnected related to the user
+        
         $token = Auth::user();
         
-        // records where status is [connected or disconnected] and requester or responder are the current user
+        // records where status is [accepted or disconnected] and requester or responder are the current user
         $connections = Connection::whereIn('status', ['accepted', 'disconnected']) -> where (function ($query) use ($token)  {
             $query -> where('requester', $token->id)->orWhere('responder', $token->id);
         })->get();
@@ -34,27 +35,43 @@ class ConnectionsController extends Controller
     }
 
     public function send_request(Request $request){
+        // sends a request only when users don't have a connection or if their previous connections are [rejected, disconnected]
+        // if status is [pending, connected] request will not be sent
 
         $request -> validate([
             "email" => "required|max:100"
         ]);
 
         $email_exists = User::where('email', $request->email)->get();
+
         if (isset($email_exists[0])){
+            // if status is rejected or disconnected I want to be able to send again
             $token = Auth::user();
             $requester_id = $token->id;
             $responder_id = $email_exists[0]->id;
-            
-            $connection = Connection::create([
-                "requester" => $requester_id,
-                "responder" => $responder_id,
-            ]);
 
-            return response()->json([
-                "status" => "success",
-                "message" =>  "Request has been sent. Good Luck!",
-                "connection" => $connection
-            ]);
+            // records where connection exists and status is [accepted or pending] => i only want to request when status [rejected, desconnected] 
+            $connection_exists = Connection::whereIn('status', ['pending', 'accepted']) -> where (function ($query) use ($requester_id, $responder_id) {
+                $query -> where(["requester"=>$requester_id , "responder"=>$responder_id]) -> orWhere(["responder"=>$requester_id , "requester"=>$responder_id]);
+            })->get();
+
+            if (isset($connection_exists[0])) {
+                return response()->json([
+                    "status" => "failed",
+                    "message" =>  "You can only send a request if you were rejected or disconncted",
+                ]);
+            } else {
+                $connection = Connection::create([
+                    "requester" => $requester_id,
+                    "responder" => $responder_id,
+                ]);
+
+                return response()->json([
+                    "status" => "success",
+                    "message" =>  "Request has been sent. Good Luck!",
+                    "connection" => $connection
+                ]);
+            }
             
         } else {
             return response()->json([
