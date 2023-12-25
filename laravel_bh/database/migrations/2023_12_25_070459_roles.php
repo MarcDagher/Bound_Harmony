30251d83289c67c39db80421efbfca8f916cdb1b
MarcDagher <marcdagher47@gmail.com>
2023-12-25 09:11:23 +0200

    [Backend] Roles Migration

--- laravel_bh/database/migrations/2023_12_25_070459_roles.php
+++ laravel_bh/database/migrations/2023_12_25_070459_roles.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        // username - email - password - location - virthdate - image - connection_status - couple_survey_status - Roles_id - created_at
+        Schema::create('roles', function(Blueprint $table){
+            $table -> bigInteger('id') -> primary();
+            $table -> string('role');
         });
     }
 
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        //
     }
 };
