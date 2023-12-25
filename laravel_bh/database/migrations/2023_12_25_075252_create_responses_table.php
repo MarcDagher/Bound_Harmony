07f605b7a9604ecebf80d7238e88c5eee3712020
MarcDagher <marcdagher47@gmail.com>
2023-12-25 09:56:19 +0200

    responses migration

---
+++ laravel_bh/database/migrations/2023_12_25_075252_create_responses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->bigInteger('id')->primary();
+            $table->bigInteger('user_id');
+            $table->bigInteger('question_id');
+            $table->string('response');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('question_id')->references('id')->on('questions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('responses');
+    }
+};
