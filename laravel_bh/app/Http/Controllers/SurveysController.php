39fc9868afdcd98a89cb2b16ebc53f78ad40f97e
MarcDagher <marcdagher47@gmail.com>
2023-12-26 09:42:21 +0200

    SurveyResponse Model

---
+++ laravel_bh/app/Http/Controllers/SurveysController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SurveysController extends Controller
+{
+    
+    public function get_survey () {
+        // query questions and options of the survey
+    }
+
+    public function send_responses () {
+        // send user's responses
+        
+        
+    }
+}
