17365f9d2d5e632aad9d485fcc65964507baff91
MarcDagher <marcdagher47@gmail.com>
2023-12-27 07:51:55 +0200

    Login Screen inputs and title

--- flutter_app/lib/screens/login_screen.dart
+++ flutter_app/lib/screens/login_screen.dart
@@ -7,17 +7,48 @@ class LogInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          Image.asset("assets/login_logo.png"),
-          Text(
-            'Log In',
-            style: TextStyle(
-                fontSize: 45,
-                color: Theme.of(context).hintColor,
-                fontWeight: FontWeight.w700),
-          ),
-        ],
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          children: [
+            ///////////////////// Logo + Title ///////////////////////
+            Image.asset("assets/login_logo.png"),
+            Text(
+              'Log In',
+              style: TextStyle(
+                  fontSize: 55,
+                  color: Theme.of(context).hintColor,
+                  fontWeight: FontWeight.w600),
+            ),
+
+            ///////////////////// INPUT FIELD  ///////////////////////
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Theme.of(context).hintColor)),
+              child: TextFormField(
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Email',
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 17))),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Theme.of(context).hintColor)),
+              child: TextFormField(
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Password',
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 17))),
+            ),
+          ],
+        ),
       )),
     );
   }
