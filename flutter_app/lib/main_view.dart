e60687fa4a3e0645581a241675c6d1dcac7c4116
MarcDagher <marcdagher47@gmail.com>
2023-12-29 14:33:25 +0200

    adjustments to the main view navbar

--- flutter_app/lib/main_view.dart
+++ flutter_app/lib/main_view.dart
@@ -13,6 +13,8 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  /// method for handling navigation branches
+  ///
   void changeBranch(int index) {
     widget.navigationShell.goBranch(
       index,
@@ -35,26 +37,59 @@ class _MainViewState extends State<MainView> {
     ///
     // if (hasAccount == true && loggedIn == true) {
     return Scaffold(
+      /// depending on The shell branch
+      ///
       body: widget.navigationShell,
-      bottomNavigationBar: SlidingClippedNavBar(
-        backgroundColor: Colors.white,
-        onButtonPressed: (index) {
+
+      /// Shell branch handler is in navbar
+      ///
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color.fromARGB(255, 233, 232, 232),
+        selectedItemColor: const Color.fromARGB(255, 243, 67, 67),
+        unselectedItemColor: const Color.fromARGB(255, 190, 186, 186),
+        selectedLabelStyle:
+            TextStyle(color: const Color.fromARGB(255, 190, 186, 186)),
+        currentIndex: selectedIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.border_color_outlined), label: 'Surveys'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.assistant), label: 'Suggestions'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.all_inclusive_sharp), label: 'Advice'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_rounded), label: 'Profile')
+        ],
+        onTap: (int newIndex) {
           setState(() {
-            selectedIndex = index;
+            selectedIndex = newIndex;
           });
           changeBranch(selectedIndex);
         },
-        iconSize: 30,
-        activeColor: Colors.black,
-        selectedIndex: selectedIndex,
-        barItems: [
-          BarItem(title: 'Surveys', icon: Icons.border_color_outlined),
-          BarItem(title: 'Suggestions', icon: Icons.assistant),
-          BarItem(title: 'Advice', icon: Icons.all_inclusive_sharp),
-          BarItem(title: 'Profile', icon: Icons.account_circle_rounded)
-        ],
       ),
     );
     // }
   }
 }
+
+// SlidingClippedNavBar(
+//         backgroundColor: Colors.white,
+//         onButtonPressed: (index) {
+//           setState(() {
+//             selectedIndex = index;
+//           });
+//           changeBranch(selectedIndex);
+//         },
+//         iconSize: 30,
+//         activeColor: Colors.black,
+//         selectedIndex: selectedIndex,
+//         barItems: [
+//           BarItem(title: 'Surveys', icon: Icons.border_color_outlined),
+//           BarItem(title: 'Suggestions', icon: Icons.assistant),
+//           BarItem(title: 'Advice', icon: Icons.all_inclusive_sharp),
+//           BarItem(title: 'Profile', icon: Icons.account_circle_rounded)
+//         ],
+//         fontSize: 16,
+//         fontWeight: FontWeight.w800,
+//       ),
