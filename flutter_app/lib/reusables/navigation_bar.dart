b7cf2d52e4f2f20e9d7338ca200470f3dbd6002e
MarcDagher <marcdagher47@gmail.com>
2023-12-29 14:57:45 +0200

    Change NavBar Styling

--- flutter_app/lib/reusables/navigation_bar.dart
+++ flutter_app/lib/reusables/navigation_bar.dart
@@ -21,27 +21,34 @@ class _MyNavigationBarState extends State<MyNavigationBar> {
     );
   }
 
-  int selectedIndex = 0;
+  int selectedIndex = 3;
 
   @override
   Widget build(BuildContext context) {
-    return SlidingClippedNavBar(
-      backgroundColor: Colors.white,
-      onButtonPressed: (index) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: const Color.fromARGB(255, 233, 232, 232),
+      selectedItemColor: const Color.fromARGB(255, 243, 67, 67),
+      unselectedItemColor: const Color.fromARGB(255, 190, 186, 186),
+      selectedLabelStyle:
+          const TextStyle(color: Color.fromARGB(255, 190, 186, 186)),
+      currentIndex: selectedIndex,
+      items: const [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.border_color_outlined), label: 'Surveys'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.assistant), label: 'Suggestions'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.all_inclusive_sharp), label: 'Advice'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded), label: 'Profile')
+      ],
+      onTap: (int newIndex) {
         setState(() {
-          selectedIndex = index;
+          selectedIndex = newIndex;
         });
         changeBranch(selectedIndex);
       },
-      iconSize: 30,
-      activeColor: Colors.black,
-      selectedIndex: selectedIndex,
-      barItems: [
-        BarItem(title: 'Surveys', icon: Icons.border_color_outlined),
-        BarItem(title: 'Suggestions', icon: Icons.assistant),
-        BarItem(title: 'Advice', icon: Icons.all_inclusive_sharp),
-        BarItem(title: 'Profile', icon: Icons.account_circle_rounded)
-      ],
     );
   }
 }
@@ -59,8 +66,8 @@ class _MyNavigationBarState extends State<MyNavigationBar> {
 //       selectedItemColor: const Color.fromARGB(255, 243, 67, 67),
 //       unselectedItemColor: const Color.fromARGB(255, 190, 186, 186),
 //       selectedLabelStyle:
-//           TextStyle(color: const Color.fromARGB(255, 190, 186, 186)),
-//       currentIndex: currentIndex,
+//           const TextStyle(color: const Color.fromARGB(255, 190, 186, 186)),
+//       currentIndex: selectedIndex,
 //       items: const [
 //         BottomNavigationBarItem(
 //             icon: Icon(Icons.border_color_outlined), label: 'Surveys'),
@@ -73,8 +80,29 @@ class _MyNavigationBarState extends State<MyNavigationBar> {
 //       ],
 //       onTap: (int newIndex) {
 //         setState(() {
-//           currentIndex = newIndex;
+//           selectedIndex = newIndex;
 //         });
-//         context.go(routes[currentIndex]);
+//         changeBranch(selectedIndex);
 //       },
 //     );
+
+
+
+// SlidingClippedNavBar(
+//       backgroundColor: Colors.white,
+//       onButtonPressed: (index) {
+//         setState(() {
+//           selectedIndex = index;
+//         });
+//         changeBranch(selectedIndex);
+//       },
+//       iconSize: 30,
+//       activeColor: Colors.black,
+//       selectedIndex: selectedIndex,
+//       barItems: [
+//         BarItem(title: 'Surveys', icon: Icons.border_color_outlined),
+//         BarItem(title: 'Suggestions', icon: Icons.assistant),
+//         BarItem(title: 'Advice', icon: Icons.all_inclusive_sharp),
+//         BarItem(title: 'Profile', icon: Icons.account_circle_rounded)
+//       ],
+//     )
