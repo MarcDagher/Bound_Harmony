946da96af340d354b4e96b268a96291cd73f625b
MarcDagher <marcdagher47@gmail.com>
2023-12-29 10:27:03 +0200

    Done BottomNavBar navigations

--- flutter_app/lib/reusables/navigation_bar.dart
+++ flutter_app/lib/reusables/navigation_bar.dart
@@ -1,5 +1,5 @@
-import 'package:bound_harmony/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MyNavigationBar extends StatefulWidget {
   const MyNavigationBar({super.key});
@@ -9,15 +9,17 @@ class MyNavigationBar extends StatefulWidget {
 }
 
 class _MyNavigationBarState extends State<MyNavigationBar> {
-  // final List screens = [SurveysScreen(), SuggestionsScreen(), AdviceScreen(), ProfileScreen()];
+  final List routes = ['/surveys', '/suggestions', '/advice', '/'];
   int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    int routesIndex = 0;
+
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
       backgroundColor: const Color.fromARGB(255, 233, 232, 232),
-      fixedColor: const Color.fromARGB(255, 233, 98, 98),
+      fixedColor: const Color.fromARGB(255, 243, 67, 67),
       items: const [
         BottomNavigationBarItem(
             icon: Icon(Icons.border_color_outlined), label: 'Surveys'),
@@ -28,12 +30,13 @@ class _MyNavigationBarState extends State<MyNavigationBar> {
         BottomNavigationBarItem(
             icon: Icon(Icons.account_circle_rounded), label: 'Profile')
       ],
-      // selectedIndex: screens[currentIndex],
-      // onDestinationSelected: (int index) {
-      //   setState(() {
-      //     currentIndex = index
-      //   });
-      // },
+      currentIndex: routesIndex,
+      onTap: (int index) {
+        setState(() {
+          routesIndex = index;
+        });
+        context.go(routes[routesIndex]);
+      },
     );
   }
 }
