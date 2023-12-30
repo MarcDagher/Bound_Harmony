354aee0d578d03cbc119871a108180a2e07f7e7f
MarcDagher <marcdagher47@gmail.com>
2023-12-30 12:20:33 +0200

    Clean comments in IncomingRequestsScreen

--- flutter_app/lib/screens/incoming_requests_screen.dart
+++ flutter_app/lib/screens/incoming_requests_screen.dart
@@ -102,48 +102,4 @@ class _IncomingRequestsScreenState extends State<IncomingRequestsScreen> {
       ///
     }
   }
-
-// // I did it here to avoid creating a separate file for only a one time import
-//   Widget buildUserListTile(String name, String email, BuildContext context,
-//           IconData icon, Color iconColor) =>
-//       Column(
-//         children: [
-//           Container(
-//             decoration: const BoxDecoration(
-//               border: Border(
-//                   bottom: BorderSide(
-//                       width: 0.5, color: Color.fromARGB(255, 206, 179, 179))),
-//             ),
-//             child: ListTile(
-//               contentPadding:
-//                   const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-//               title: Row(
-//                 children: [
-//                   Padding(
-//                     padding: const EdgeInsets.only(right: 10),
-//                     child: Icon(
-//                       icon,
-//                       size: 35,
-//                       color: iconColor,
-//                     ),
-//                   ),
-//                   Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       Text(
-//                         name,
-//                         style: const TextStyle(fontSize: 18),
-//                       ),
-//                       Text(
-//                         email,
-//                         style: const TextStyle(fontSize: 18),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ],
-//       );
 }
