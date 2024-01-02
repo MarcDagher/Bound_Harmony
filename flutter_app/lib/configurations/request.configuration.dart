f557d64965cf81598459a522921e3b976e7288be
MarcDagher <marcdagher47@gmail.com>
2024-01-02 16:00:46 +0200

    Wrap app with Multi Provider and create user provider

---
+++ flutter_app/lib/configurations/request.configuration.dart
@@ -0,0 +1,3 @@
+class Requests {
+  static String baseUrl = "http://192.168.1.66:8000/api/";
+}
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:bound_harmony/configurations/app_router.dart';
+import 'package:bound_harmony/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,29 +12,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Bound Harmony',
-      //// Design themes + setup
-      ///
-      ///
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.white,
-            scrolledUnderElevation: 0,
-            shape: Border.symmetric(
-                horizontal: BorderSide(width: 0.2, color: Color(0xFF544B4C)))),
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: const Color(0xFFF03E3F),
-        hintColor: const Color(0xFF544B4C),
-        fontFamily: "Nunito",
-        useMaterial3: true,
-      ),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => UserProvider())],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'Bound Harmony',
+        //// Design themes + setup
+        ///
+        ///
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.white,
+              scrolledUnderElevation: 0,
+              shape: Border.symmetric(
+                  horizontal:
+                      BorderSide(width: 0.2, color: Color(0xFF544B4C)))),
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: const Color(0xFFF03E3F),
+          hintColor: const Color(0xFF544B4C),
+          fontFamily: "Nunito",
+          useMaterial3: true,
+        ),
 
-      //// Go Router
-      ///
-      ///
-      routerConfig: AppNavigation.router,
+        //// Go Router
+        ///
+        ///
+        routerConfig: AppNavigation.router,
+      ),
     );
   }
 }
