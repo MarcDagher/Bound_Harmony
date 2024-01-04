821f95c195b0da404bffd02590ab026f5a5b2d94
MarcDagher <marcdagher47@gmail.com>
2024-01-04 15:59:23 +0200

    [Cleaning] Remove comments in AuthProvider

--- flutter_app/lib/providers/auth_provider.dart
+++ flutter_app/lib/providers/auth_provider.dart
@@ -3,7 +3,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class AuthProvider extends ChangeNotifier {
-  // the with keyword allows us to create a class AuthProvider while mixing in the methods and properties of ChangeNotifier without creating a strict inheritance hierarchy. We are not creating a subclass of ChangeNotifier, we are creating a mixin AuthProvider which takes the methods and properties of ChangeNotifier and can also mix in other features from other classes
   final dio = Dio();
   bool? success;
   bool? emailTaken;
@@ -20,7 +19,7 @@ class AuthProvider extends ChangeNotifier {
         "password": formPassword,
         "birthdate": "15-01-2003",
       });
-      print("from provider => response data: ${response.data}");
+      // print("from provider => response data: ${response.data}");
 
       if (response.data['status'] == "success") {
         success = true;
@@ -31,11 +30,11 @@ class AuthProvider extends ChangeNotifier {
         emailTaken = true;
         success = false;
       }
-      print("from provider => status code: ${e.response!.statusCode}");
+      // print("from provider => status code: ${e.response!.statusCode}");
     }
 
-    print("from provider => success: $success");
-    print("from provider => emailTaken: $emailTaken");
+    // print("from provider => success: $success");
+    // print("from provider => emailTaken: $emailTaken");
     // notifyListeners();
     return [emailTaken, success];
   }
