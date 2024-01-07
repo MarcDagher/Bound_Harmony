8c390b7db3345b37e16b21da952a66258713c7ca
MarcDagher <marcdagher47@gmail.com>
2024-01-07 11:59:40 +0200

    [Flutter Fetching] Add response messages and transition button to ConnectionSetupScreen

--- flutter_app/lib/providers/connection_provider.dart
+++ flutter_app/lib/providers/connection_provider.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class ConnectionProvider extends ChangeNotifier {
-  String? message;
+  String message = "";
   bool? success;
 
   Future sendRequest(String email, token) async {
@@ -15,21 +15,18 @@ class ConnectionProvider extends ChangeNotifier {
       final response = await dio.post("$baseUrl/send_request",
           data: {'email': email},
           options: Options(headers: {'authorization': 'Bearer $token'}));
-      if (response.data["status"] == 'success') {
-        success = true;
-        message = response.data["message"];
-        print(message);
-      }
+
+      success = true;
+      message = response.data["message"];
+
       // print("From ConnectionProvider response: ${response.data}");
     } on DioException catch (error) {
       if (error.response!.statusCode == 405) {
         success = false;
         message = "Request already exists";
-        print(message);
       } else if (error.response!.statusCode == 403) {
         success = false;
         message = "Email doesn't exist";
-        print(message);
       }
       // print("From ConnectionProvider error: $error");
     }
--- flutter_app/lib/screens/connection_setup_screen.dart
+++ flutter_app/lib/screens/connection_setup_screen.dart
@@ -29,56 +29,101 @@ class ConnectionSetupScreen extends StatelessWidget {
             Image.asset("assets/logo.png"), // when keyboard appears hide logo
 
             ///////////////////// Column: Titles ///////////////////////
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: Text(
-                    'Welcome To Bound Harmony!',
-                    style: TextStyle(
-                        fontSize: 26, color: Theme.of(context).hintColor),
+            Consumer<ConnectionProvider>(
+              builder: (context, value, child) => Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      'Welcome To Bound Harmony!',
+                      style: TextStyle(
+                          fontSize: 26, color: Theme.of(context).hintColor),
+                    ),
                   ),
-                ),
-                Text(
-                  "Here you will connect to your partner",
-                  style: TextStyle(
-                      fontSize: 15, color: Theme.of(context).hintColor),
-                )
-              ],
+                  if (value.success != false && value.success != true)
+                    Text(
+                      "Here you will connect to your partner",
+                      style: TextStyle(
+                          fontSize: 15, color: Theme.of(context).hintColor),
+                    ),
+                  if (value.success == false)
+                    Text(
+                      value.message,
+                      style: TextStyle(
+                          fontSize: 15, color: Theme.of(context).primaryColor),
+                    ),
+                  if (value.success == true)
+                    Text(
+                      value.message,
+                      style: TextStyle(
+                          fontSize: 15, color: Theme.of(context).primaryColor),
+                    ),
+                ],
+              ),
             ),
 
             ///////////////////// Column: Input + BUTTON ///////////////////////
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 25),
-                  child: Form(
-                    key: formKey,
-                    child: TextInputField(
-                        handleChangeController: emailController,
-                        handleValidation: (email) => email!.isEmpty
-                            ? "Email is required"
-                            : !RegExp(r'^[\w-]+(\.[\w-]+)*@(hotmail\.com|gmail\.com|yahoo\.com|outlook\.com)$')
-                                    .hasMatch(emailController.text)
-                                ? "Invalid email format"
-                                : null,
-                        placeholder: "Enter your partner's email"),
-                  ),
-                ),
-                Button(
-                  text: 'Send Request',
-                  handlePressed: () async {
-                    if (formKey.currentState!.validate()) {
-                      final token =
-                          await context.read<AuthProvider>().getToken();
-                      // print("from screen's button: $token");
-                      context
-                          .read<ConnectionProvider>()
-                          .sendRequest(emailController.text, token);
-                    }
-                  },
-                ),
-              ],
+            Consumer<ConnectionProvider>(
+              builder: (context, value, child) => Column(
+                children: [
+                  if (value.success != true)
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 25),
+                      child: Form(
+                        key: formKey,
+                        child: TextInputField(
+                            handleChangeController: emailController,
+                            handleValidation: (email) => email!.isEmpty
+                                ? "Email is required"
+                                : !RegExp(r'^[\w-]+(\.[\w-]+)*@(hotmail\.com|gmail\.com|yahoo\.com|outlook\.com)$')
+                                        .hasMatch(emailController.text)
+                                    ? "Invalid email format"
+                                    : null,
+                            placeholder: "Enter your partner's email"),
+                      ),
+                    ),
+                  if (value.success != true)
+                    Button(
+                      text: 'Send Request',
+                      handlePressed: () async {
+                        if (formKey.currentState!.validate()) {
+                          final token =
+                              await context.read<AuthProvider>().getToken();
+                          // print("from screen's button: $token");
+                          context
+                              .read<ConnectionProvider>()
+                              .sendRequest(emailController.text, token);
+                        }
+                      },
+                    ),
+                  if (value.success == true)
+                    MaterialButton(
+                        color: Theme.of(context).primaryColor,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 18, horizontal: 15),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Go to profile',
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w800,
+                                  color: Colors.white),
+                            ),
+                            Icon(
+                              Icons.navigate_next,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                        onPressed: () {
+                          context.goNamed('Profile');
+                        })
+                ],
+              ),
             ),
           ],
         ),
