d1301c91ae344cbd75ce6b3f80789f3aac6df551
MarcDagher <marcdagher47@gmail.com>
2024-01-09 21:16:14 +0200

    [Flutter Layout] Initialize Question Model

---
+++ flutter_app/lib/models/questions_model.dart
@@ -0,0 +1,13 @@
+class Question {
+  final int id;
+  final String question;
+  final List<Map> options;
+
+  Question({required this.id, required this.options, required this.question});
+
+  // override the toString method to print the questions on console
+  @override
+  String toString() {
+    return "Question(id: $id, question: $question, options: $options)";
+  }
+}
