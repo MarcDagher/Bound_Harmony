ab86e637e45d654ee72c03198865c481574f4ba6
MarcDagher <marcdagher47@gmail.com>
2024-01-09 21:38:01 +0200

    [Flutter Layout] Add loops in getSurvey method

--- flutter_app/lib/providers/survey_provider.dart
+++ flutter_app/lib/providers/survey_provider.dart
@@ -1,4 +1,5 @@
 import 'package:bound_harmony/configurations/request.configuration.dart';
+import 'package:bound_harmony/models/questions_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -13,7 +14,19 @@ class SurveysProvider extends ChangeNotifier {
       final token = preferences.get('token');
       final response =
           await dio.get("$baseUrl/get_survey", data: {"survey_id": survey_id});
-      print("In getSurvey survey: ${response.data}");
+
+      // print(
+      //     "In getSurvey survey: ${response.data["survey"][0]["question"]["question"]}");
+      // print("In getSurvey survey");
+      for (int i = 0; i < response.data["survey"].length; i++) {
+        print(
+            "Question $i: ${response.data["survey"][i]["question"]["question"]}");
+        for (int j = 0; j < response.data["survey"][i]["options"].length; j++) {
+          print(
+              "Option $j: ${response.data["survey"][i]["options"][j]["option"]}");
+        }
+      }
+      // Question(id: id, options: options, question: question)
     } on DioException catch (error) {
       print("In getSurvey error: $error");
     }
