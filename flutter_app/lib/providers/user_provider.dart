a124b98c6f0a7ba7d43b6953e15856fe651f695e
MarcDagher <marcdagher47@gmail.com>
2024-01-13 08:32:23 +0200

    [Flutter Fetching] Created changeLocation method in UserProvider

--- flutter_app/lib/providers/user_provider.dart
+++ flutter_app/lib/providers/user_provider.dart
@@ -3,8 +3,13 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class UserProvider extends ChangeNotifier {
+  // changeUsername method
   String newDefaultUsername = "";
   bool newUsernameSuccess = false;
+
+  // changeLocation method
+  String newDefaultLocation = "";
+  bool newLocationSuccess = false;
 
   changeUsername(token, String newUsername) async {
     final baseUrl = Requests.baseUrl;
@@ -20,7 +25,27 @@ class UserProvider extends ChangeNotifier {
         notifyListeners();
       }
 
-      print("in updateProfileInfo: ${response.data}");
+      print("in changeUsername: ${response.data}");
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  changeLocation(token, newLocation) async {
+    final baseUrl = Requests.baseUrl;
+    final dio = Dio();
+
+    try {
+      final response = await dio.post('$baseUrl/change_location',
+          data: {"location": newLocation},
+          options: Options(headers: {"authorization": "Bearer $token"}));
+      if (response.data["status"] == "success") {
+        newDefaultLocation = newLocation;
+        newLocationSuccess = true;
+        notifyListeners();
+      }
+
+      print("in changeLocation: ${response.data}");
     } catch (e) {
       print(e);
     }
