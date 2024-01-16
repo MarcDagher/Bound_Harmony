70e0892fc280c240da9ab84f000521cd5fbdf66e
MarcDagher <marcdagher47@gmail.com>
2024-01-16 09:01:07 +0200

    [Laravel Controllers] Created get_conversation() V0 in MessagesController

--- laravel_bh/app/Http/Controllers/MessagesController.php
+++ laravel_bh/app/Http/Controllers/MessagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AiResponse;
 use App\Models\UserPrompt;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,10 +13,38 @@ class MessagesController extends Controller
 
     public function get_conversation() {
         $user = Auth::user();
-        // fetch messages from ai_responses
+        // fetch user_prompts + ai_responses 
+        $messages = AiResponse::where('user_prompt_id', $user->id) -> get();
+
+        if (count($messages) > 0){
+            $ordered_conversation = [];
+            foreach ($messages as $message){
+                $ordered_conversation[] = [
+                    // user_prompt_in_ai_responses is in AiResponse model realtions
+                    "user_prompt" => $message -> user_prompt_in_ai_responses -> prompt,
+                    "user_prompt_date" => $message -> user_prompt_in_ai_responses -> created_at,
+                    "ai_response" => $message -> response,
+                    "ai_response_date" => $message -> created_at
+                ];
+            }
+
+            return response() -> json([
+                "status" => "success",
+                "conversation" => $ordered_conversation
+            ]);
+
+        } else {
+            return response() -> json([
+                "status" => "rejected",
+                "message" => "Empty conversation"
+            ]);
+        }
     }
 
 
+    // Save user prompts, then send request to OpenAi.
+    // Receive response from OpenAi
+    // Return ai_response to the user
     public function save_user_prompt (Request $request) {
         // save prompt in user_prompts 
         
@@ -23,6 +52,7 @@ class MessagesController extends Controller
         $token = Auth::user();
 
         try {
+
             $prompt = UserPrompt::create([
                 "user_id" => $token -> id,
                 "prompt" => $request -> prompt
@@ -33,6 +63,7 @@ class MessagesController extends Controller
                 "message" => "Awaiting response",
                 "user_prompt" => $prompt
             ]);        
+
         } catch (\Exception $e) {
             return response() -> json([
                 "status" => "failed",
