85ff53073a2b52d8bd7fdd0404bdf927ff38d998
MarcDagher <marcdagher47@gmail.com>
2024-01-18 16:23:52 +0200

    [Laravel Controllers] Added users and partners answers of Q28 to list (as query parameters) SurveysControllers

--- laravel_bh/app/Http/Controllers/SuggestionsController.php
+++ laravel_bh/app/Http/Controllers/SuggestionsController.php
@@ -32,45 +32,66 @@ class SuggestionsController extends Controller
 
     public function get_couple_survey_Q_responses($user_id, $connection_id, $question_id){
         $user_Q_couple_survey_response = SurveyResponse::where(['connection_id' => $connection_id, 'user_id' => $user_id, "question_id" => $question_id]) 
-                                        -> with('option') -> get('option_id');
+                                        -> get('option_id');
         return $user_Q_couple_survey_response;
     }
 
-    //I need to know th user's interests to know what to suggest.
-    //User has to have a partner in order to get to here.
     
     public function get_suggestions(){
         $user = Auth::user();
         $couples_combined_interests = []; // answers of Q28 + user's + partner's(if Q25 is yes)
-        //get the connection id of this user's relationship
+
+        //get the connection id and partner id of this user's relationship
         $connection_and_partner_ids = $this -> search_for_connection_and_partner($user);
         $connection_id = $connection_and_partner_ids['connection_id'];
         $partner_id = $connection_and_partner_ids['partner_id'];
 
-        //get Couple Survey Responses of the user (Q = question)
+        //get Couple Survey Responses of Q28 (Q = question) of the user and the partner 
         $user_Q28_couple_survey_response = $this -> get_couple_survey_Q_responses($user -> id, $connection_id, 28);
         $partner_Q28_couple_survey_response = $this -> get_couple_survey_Q_responses($partner_id, $connection_id, 28);
 
+        // activities which the user enjoys with their partner 
         foreach ($user_Q28_couple_survey_response as $option) {
-            if (!in_array($option['option']['option'], $couples_combined_interests)){
-                array_push($couples_combined_interests, $option['option']['option']);    
+            switch ($option){
+                case ($option["option_id"] == "93") : array_push($couples_combined_interests, "outdoor", "nature"); break;
+                case ($option["option_id"] == "94") : array_push($couples_combined_interests, "social", "entertainment"); break;
+                case ($option["option_id"] == "95") : array_push($couples_combined_interests, "sports", "physical activity"); break;
+                case ($option["option_id"] == "96") : array_push($couples_combined_interests, "yoga", "spa"); break;
+                case ($option["option_id"] == "97") : array_push($couples_combined_interests, "meditation"); break;
+                case ($option["option_id"] == "98") : array_push($couples_combined_interests, "museum", "history"); break;
+                case ($option["option_id"] == "99") : array_push($couples_combined_interests, "movies", "amusement"); break;
             }
         }
+
+        // activities which the partner enjoys with the user 
         foreach ($partner_Q28_couple_survey_response as $option) {
-            if (!in_array($option['option']['option'], $couples_combined_interests)){
-                array_push($couples_combined_interests, $option['option']['option']);    
+            switch ($option){
+                case ($option["option_id"] == "93") : array_push($couples_combined_interests, "outdoor", "nature"); break;
+                case ($option["option_id"] == "94") : array_push($couples_combined_interests, "social", "entertainment"); break;
+                case ($option["option_id"] == "95") : array_push($couples_combined_interests, "sports", "physical activity"); break;
+                case ($option["option_id"] == "96") : array_push($couples_combined_interests, "yoga", "spa"); break;
+                case ($option["option_id"] == "97") : array_push($couples_combined_interests, "meditation"); break;
+                case ($option["option_id"] == "98") : array_push($couples_combined_interests, "museum", "history"); break;
+                case ($option["option_id"] == "99") : array_push($couples_combined_interests, "movies", "amusement"); break;
             }
         }
 
         // if user answers yes to Q25 : open to experiencing my partner's activities and interests
         $user_Q25_couple_survey_response = SurveyResponse::where(["user_id" => $user -> id, "connection_id" => $connection_id, "question_id" => 25])
                                           -> with('option') -> latest() -> first('option_id')['option']['option'];
-        // if ($user_Q25_couple_survey_response == "yes"){
-        //     // add partners interests
-        // }
+        if ($user_Q25_couple_survey_response == "yes"){
+            // add partners interests
+        }
 
         // add my interests
-        // $my_interests = SurveyResponse::where(["user_id" => $user -> id]) -> with('option');
+        $my_interests = SurveyResponse::where(["user_id" => $user -> id, "survey_id" => 1]) -> with('question', 'option') -> get(['question_id', 'option_id']);
+        foreach($my_interests as $interest){
+            // echo $interest['question']['question'] .': '. $interest['option']['option'] ."\n";   
+            // echo $interest; 
+            // issue now is, how will I extract a query from the question. I need to assign a query or a type, to each interest
+        }
+
+        print_r(array_unique($couples_combined_interests));
                 
     }
 }
