d73e5f61c199f692c97e2e1a136d6f2cbb8e8dfa
MarcDagher <marcdagher47@gmail.com>
2024-01-18 17:18:12 +0200

    [Laravel Controllers] Added helper methods in SurveysControllers [V0]

--- laravel_bh/app/Http/Controllers/SuggestionsController.php
+++ laravel_bh/app/Http/Controllers/SuggestionsController.php
@@ -10,8 +10,8 @@ use Illuminate\Support\Facades\Auth;
 class SuggestionsController extends Controller
 {
 
-     // helper method 
-     public function search_for_connection_and_partner($user){
+    // helper method 
+    public function search_for_connection_and_partner($user){
         // search for user's partner
         $connection_where_partner_is_requester = Connection::where(['status' => 'accepted', "responder" => $user -> id]) -> get() -> first();
         $connection_where_partner_is_responder = Connection::where(['status' => 'accepted', "requester" => $user -> id]) -> get() -> first();
@@ -30,10 +30,37 @@ class SuggestionsController extends Controller
         }
     }
 
+    // helper method
     public function get_couple_survey_Q_responses($user_id, $connection_id, $question_id){
         $user_Q_couple_survey_response = SurveyResponse::where(['connection_id' => $connection_id, 'user_id' => $user_id, "question_id" => $question_id]) 
                                         -> get('option_id');
         return $user_Q_couple_survey_response;
+    }
+
+    // helper method
+    public function filter_Q25_responses($interest){
+        switch($interest){ // consider the opposite case. if crowded = no then calm
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 1:  return "crowded"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 2:  return "nightlife"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 3:  return "outdoor"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 4:  return "nature"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 5:  return ["museum", "art gallery"]; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 6:  return ["culture", "education"]; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 7:  return "book"; break; 
+                case $interest['option_id'] % 2 != 0 && $interest['question_id'] == 8:  return "religion"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 9:  return "calm"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 10:  return "spiritual"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 11:  return "self-care"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 12:  return "shopping"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 13:  return "exercise"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 14:  return "restaurant"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 15:  return "shopping"; break; 
+                case $interest['option_id'] % 2 != 0 && $interest['question_id'] == 16:  return "shopping"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 17:  return "sports"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 18:  return "home activities"; break; 
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 19:  return "movie"; break;  
+                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 20:  return "animals"; break;  
+            }
     }
 
     
@@ -41,11 +68,11 @@ class SuggestionsController extends Controller
         $user = Auth::user();
         $couples_combined_interests = []; // answers of Q28 + user's + partner's(if Q25 is yes)
 
-        //get the connection id and partner id of this user's relationship
+        //get the connection id and partner id of this user's relationship (connection_id)
         $connection_and_partner_ids = $this -> search_for_connection_and_partner($user);
         $connection_id = $connection_and_partner_ids['connection_id'];
         $partner_id = $connection_and_partner_ids['partner_id'];
-
+        
         //get Couple Survey Responses of Q28 (Q = question) of the user and the partner 
         $user_Q28_couple_survey_response = $this -> get_couple_survey_Q_responses($user -> id, $connection_id, 28);
         $partner_Q28_couple_survey_response = $this -> get_couple_survey_Q_responses($partner_id, $connection_id, 28);
@@ -80,37 +107,28 @@ class SuggestionsController extends Controller
         $user_Q25_couple_survey_response = SurveyResponse::where(["user_id" => $user -> id, "connection_id" => $connection_id, "question_id" => 25])
                                           -> with('option') -> latest() -> first('option_id')['option']['option'];
 
-        // echo $user_Q25_couple_survey_response; 
-        if ($user_Q25_couple_survey_response == "yes"){
+        // if user is interested in trying their partner's interests
+        if ($user_Q25_couple_survey_response == "Yes"){
+            $partner_interests = SurveyResponse::where(["user_id" => $partner_id, "survey_id" => 1]) -> get(['question_id', 'option_id']);
 
-            // add partners interests
+            foreach($partner_interests as $interest){
+                $value = $this -> filter_Q25_responses($interest);
+                if (is_array($value)){
+                    foreach($value as $new_value)
+                    array_push($couples_combined_interests, $new_value);
+                } elseif (is_string($value)){array_push($couples_combined_interests, $value);}
+            }
         }
 
-        // add user interests (as google places queries) to $couples_combined_interests
+        // add user interests to $couples_combined_interests
         $my_interests = SurveyResponse::where(["user_id" => $user -> id, "survey_id" => 1]) -> get(['question_id', 'option_id']);
+
         foreach($my_interests as $interest){
-            switch($interest){ // consider the opposite case. if crowded = no then calm
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 1: array_push($couples_combined_interests, "crowded"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 2: array_push($couples_combined_interests, "nightlife"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 3: array_push($couples_combined_interests, "outdoor"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 4: array_push($couples_combined_interests, "nature"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 5: array_push($couples_combined_interests, "museum", "art gallery"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 6: array_push($couples_combined_interests, "culture", "education"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 7: array_push($couples_combined_interests, "book"); break; 
-                case $interest['option_id'] % 2 != 0 && $interest['question_id'] == 8: array_push($couples_combined_interests, "religion"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 9: array_push($couples_combined_interests, "calm"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 10: array_push($couples_combined_interests, "spiritual"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 11: array_push($couples_combined_interests, "self-care"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 12: array_push($couples_combined_interests, "shopping"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 13: array_push($couples_combined_interests, "exercise"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 14: array_push($couples_combined_interests, "restaurant"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 15: array_push($couples_combined_interests, "shopping"); break; 
-                case $interest['option_id'] % 2 != 0 && $interest['question_id'] == 16: array_push($couples_combined_interests, "shopping"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 17: array_push($couples_combined_interests, "sports"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 18: array_push($couples_combined_interests, "home activities"); break; 
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 19: array_push($couples_combined_interests, "movie"); break;  
-                case $interest['option_id'] % 2 == 0 && $interest['question_id'] == 20: array_push($couples_combined_interests, "animals"); break;  
-            }
+            $value = $this -> filter_Q25_responses($interest);
+            if (is_array($value)){
+                foreach($value as $new_value)
+                array_push($couples_combined_interests, $new_value);
+            } elseif (is_string($value)){array_push($couples_combined_interests, $value);}
         }
 
         print_r(array_unique($couples_combined_interests));
