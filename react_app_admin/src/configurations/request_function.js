af80a13b707c11c821d4d1d848eb6dee1b53d104
MarcDagher <marcdagher47@gmail.com>
2024-01-21 17:43:46 +0200

    [React Fetchings] Setup axios helper function

---
+++ react_app_admin/src/configurations/request_function.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+
+axios.defaults.baseURL = "http://192.168.1.66:8000/api";
+const send_request = async ({route, method = "GET", body}) => {
+  try {
+
+      const response = axios.request( {
+        url: route,
+        method: method,
+        data: body,
+        headers: {"Content-Type" : "application/json"}
+      } );
+      return response;    
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default send_request;
