7dc728ab1fe8add2e5cba90c8b0cef3cd81a3b8f
MarcDagher <marcdagher47@gmail.com>
2024-01-21 20:19:20 +0200

    [React layout] Basic login layout

--- react_app_admin/src/pages/login_page/login.jsx
+++ react_app_admin/src/pages/login_page/login.jsx
@@ -3,8 +3,29 @@ const Login = () => {
 
 
   return <>
-    <p>hello</p>
-  
+  <div className="wrapper">
+
+    <div className="container">
+
+        <div className="box-container">
+          <p>Log In</p>
+          <div className="input-container">
+            <input type="text" name="email" id="email" placeholder="email"/>
+            <input type="password" name="password" id="password" placeholder="password"/>
+          </div>
+          <button type="button">Log In</button>
+        </div>
+
+        <div className="image-container-box">
+          <img src="../.././public/Logo 2.png" alt="img" />
+          <div className="image-container">
+            <p>Bound Harmony</p>
+          </div>
+        </div>
+
+    </div>
+
+  </div>
   </>
 }
 
