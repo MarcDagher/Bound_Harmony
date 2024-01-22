57a1a754c8867db3f9504efb7f939bd22058ff17
MarcDagher <marcdagher47@gmail.com>
2024-01-22 15:59:36 +0200

    [React Routin] Initialized SurveyResponses Page

--- react_app_admin/src/App.js
+++ react_app_admin/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/login_page/login"
 import Dashboard from "./pages/dashboard_page/dashboard_page";
 import Users from "./pages/users_page/UsersPage";
+import SurveyResponses from "./pages/survey_responses_page/SurveyResponses";
 function App() {
   return <>
   <BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Login />}></Route>
       <Route path="/dashboard" element={<Dashboard />}></Route>
       <Route path="/users" element={<Users />}></Route>
+      <Route path="/surveysResponses" element={<SurveyResponses />}></Route>
     </Routes>
   </BrowserRouter>
   </>
---
+++ react_app_admin/src/pages/survey_responses_page/SurveyResponses.jsx
@@ -0,0 +1,13 @@
+import SideBar from "../../components/SideBar/SideBar"
+import './SurveyResponses.css'
+const SurveyResponses = () => {
+
+  
+  return <>
+  <div className="survey-reponses-wrapper">
+    {<SideBar />}
+  </div>
+  </>
+}
+
+export default SurveyResponses
