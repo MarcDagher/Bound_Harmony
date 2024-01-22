d0d100e16f487834412606ad50acb9440051bb1c
MarcDagher <marcdagher47@gmail.com>
2024-01-22 14:20:16 +0200

    [React Routing] Initialize Users component and route

--- react_app_admin/src/App.js
+++ react_app_admin/src/App.js
@@ -1,12 +1,14 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/login_page/login"
 import Dashboard from "./pages/dashboard_page/dashboard_page";
+import Users from "./pages/users_page/UsersPage";
 function App() {
   return <>
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />}></Route>
       <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route path="/users" element={<Users />}></Route>
     </Routes>
   </BrowserRouter>
   </>
