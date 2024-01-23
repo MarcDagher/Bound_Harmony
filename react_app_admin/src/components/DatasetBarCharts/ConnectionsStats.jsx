24008be9761513d04364a44331239db0972f92a2
MarcDagher <marcdagher47@gmail.com>
2024-01-23 20:06:35 +0200

    [React Layout] Change Survey bar chart's color

--- react_app_admin/src/components/DatasetBarCharts/ConnectionsStats.jsx
+++ react_app_admin/src/components/DatasetBarCharts/ConnectionsStats.jsx
@@ -31,17 +31,18 @@ export default function ConnectionsStats({connectionAndSurveyStats}) {
     },
   ];
 
-  // console.log("Inside function")
-  // console.log(connectionAndSurveyStats)
+
+  const seriesColors = [ '#FF7F80', '#B52022', '#F03E3F', '#F45A5C'];
+
   return (
     <BarChart
       dataset={dataset}
       xAxis={[{ scaleType: 'band', dataKey: 'month' }]}
       series={[
-        { dataKey: 'accepted_connections', label: 'Accepted', valueFormatter },
-        { dataKey: 'disconnected_connections', label: 'Disconnected', valueFormatter },
-        { dataKey: 'pending_connections', label: 'Pending', valueFormatter },
-        { dataKey: 'rejected_connections', label: 'Rejected', valueFormatter },
+        { dataKey: 'accepted_connections', label: 'Accepted', valueFormatter, color: seriesColors[0] },
+        { dataKey: 'disconnected_connections', label: 'Disconnected', valueFormatter, color: seriesColors[1]},
+        { dataKey: 'pending_connections', label: 'Pending', valueFormatter, color: seriesColors[2]},
+        { dataKey: 'rejected_connections', label: 'Rejected', valueFormatter, color: seriesColors[3]},
       ]}
       {...chartSetting}
     />
