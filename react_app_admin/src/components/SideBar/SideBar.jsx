8f97685ada9a4a7e2536498f50f66d7e41538b34
MarcDagher <marcdagher47@gmail.com>
2024-01-23 08:47:02 +0200

    [React Layout] Added highlighted background color for selected element in sidebar

--- react_app_admin/src/components/SideBar/SideBar.jsx
+++ react_app_admin/src/components/SideBar/SideBar.jsx
@@ -1,11 +1,13 @@
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import "./SideBar.css" 
 import send_request from "../../configurations/request_function"
+import { useState } from "react"
 const SideBar = () => {
 
   const navigate = useNavigate()
-  
+  const location = useLocation()
   // background-color: rgb(56, 56, 56);
+  const currentLocation = location.pathname.split('/')[1]
 
   const logout = async () => {
     const token = localStorage.getItem('token')
@@ -23,20 +25,28 @@ const SideBar = () => {
     }
   }
 
+  
+  console.log(currentLocation)
+
   return <>
       <div className='admin-sidebar'>
-        <div className="dashboard">
+
+        <div className="dashboard" style={{backgroundColor: currentLocation === "dashboard" ? "rgb(56, 56, 56)" : null}}>
             <span onClick={() => {navigate('/dashboard')}  }>Dashboard</span>
         </div>
-        <div className="users">
+
+        <div className="users" style={{backgroundColor: currentLocation === "users" ? "rgb(56, 56, 56)" : null}}>
             <span onClick={() => navigate('/users')}>Users</span>
         </div>
-        <div className="survey-responses">
+
+        <div className="survey-responses" style={{backgroundColor: currentLocation === "survey_responses" ? "rgb(56, 56, 56)" : null}}>
             <span onClick={() => navigate('/survey_responses')}>Survey Responses</span>
         </div>
+
         <div className="logout">
             <span onClick={() => logout()}>Logout</span>
         </div>
+
     </div>
   </>
 }
