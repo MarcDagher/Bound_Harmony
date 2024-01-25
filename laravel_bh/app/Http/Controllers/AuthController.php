2f0becfd19e4ac0cfe14c43186765e49156cee0d
MarcDagher <marcdagher47@gmail.com>
2024-01-25 11:20:45 +0200

    [Laravel Controllers] Fixed bug in AuthController logout

--- laravel_bh/app/Http/Controllers/AuthController.php
+++ laravel_bh/app/Http/Controllers/AuthController.php
@@ -103,20 +103,24 @@ class AuthController extends Controller
 
     public function logout()
     {
-        $user = Auth::user();
+        try {
+            $user = Auth::user();
         
-        if ($user -> first_login == true){
-            $user_in_db = User::find($user->id);
-            $user_in_db -> first_login = false;
-            $user_in_db -> save();
-        }
+            if ($user -> first_login == true){
+                $user_in_db = User::find($user->id);
+                $user_in_db -> first_login = false;
+                $user_in_db -> save();
+            }
 
-        Auth::logout();
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Successfully logged out',
-            'user' => $user_in_db
-        ]);
+            Auth::logout();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Successfully logged out',
+            ]);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+        
     }
 
     public function refresh()
