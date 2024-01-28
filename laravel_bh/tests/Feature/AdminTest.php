f66b9e6bb969c5167c7a680c4bb9426332186d45
MarcDagher <marcdagher47@gmail.com>
2024-01-28 21:28:45 +0200

    [Laravel Testing] Successful Admin Login

--- laravel_bh/tests/Feature/AdminTest.php
+++ laravel_bh/tests/Feature/AdminTest.php
@@ -16,7 +16,7 @@ class AdminTest extends TestCase
     use RefreshDatabase;
     
     public function test_failed_admin_login(): void {
-        $this -> create_role();
+        $this -> create_roles();
         $this -> create_user('mousa@hotmail.com');
 
         $response = $this->post('/api/signin', ['email' => 'mousa@hotmail.com', "password" => "password"]);
@@ -24,17 +24,24 @@ class AdminTest extends TestCase
         $response->assertStatus(401);
     }
 
-    public function create_role(){
+    public function test_admin_login_success(){
+        $this->create_roles();
+        $this->create_user('mousa@hotmail.com', 1);
+        $response = $this->post('/api/signin', ['email' => 'mousa@hotmail.com', "password" => "password"]);
+        $response->assertStatus(200);
+    }
+
+    public function create_roles(){
         Role::factory() -> create(["role" => 'admin']);
         Role::factory() -> create(["role" => 'user']);
     }
 
-    public function create_user($email){
+    public function create_user($email, $role_id = 2){
         User::factory() -> create([
+            "role_id" => $role_id,
             "email" => $email,
             "password" => Hash::make('password'),
             'birthdate' => fake()->date(), 
         ]);
     }
-
 }
