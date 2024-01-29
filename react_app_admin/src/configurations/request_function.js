a7331c3ea9d78463c1bf28131ec85f107a5972e4
MarcDagher <marcdagher47@gmail.com>
2024-01-29 07:44:48 +0200

    [Flutter/React] Adjusted Base Url

--- react_app_admin/src/configurations/request_function.js
+++ react_app_admin/src/configurations/request_function.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const send_request = async ({route, method = "GET", body , headerValue}) => {
   try {
       const response = axios.request( {
-        url: `http://192.168.1.66:8000/api${route}`,
+        url: `http://http://15.188.8.50/api${route}`,
         method: method,
         data: body,
         headers: {"Content-Type" : "application/json", "Authorization" : headerValue}
